c8ee334fa8d534417f4f81938d725b8c3c3fb310
shubham sharma <shubhsharmak2k@gmail.com>
2020-02-20 18:03:17 +0530

    Notice board impl, Jquery added

--- src/app/notice-board/notice-board.component.ts
+++ src/app/notice-board/notice-board.component.ts
@@ -1,21 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import * as $ from 'jquery'
 @Component({
   selector: 'app-notice-board',
   templateUrl: './notice-board.component.html',
   styleUrls: ['./notice-board.component.css']
 })
-export class NoticeBoardComponent implements OnInit {
+export class NoticeBoardComponent implements OnInit, AfterViewInit {
 
-  hide=true;
+  hide = true;
   constructor() {
-   }
+  }
 
   ngOnInit() {
   }
 
-  showHide(el){
-    this.hide = false; 
+  showHide(el) {
+    this.hide = false;
+  }
+
+  ngAfterViewInit(): void {
+    $('.tree-toggle').click(function () {
+      $(this).parent().children('ul.tree').toggle(200);
+    });
+    $(function () {
+      $('.tree-toggle').parent().children('ul.tree').toggle(200);
+    })
   }
 
 }
